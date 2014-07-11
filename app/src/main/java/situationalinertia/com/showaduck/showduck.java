2d3c9c6db00918c2a511ae17e3a29755c2a233f1
Johnathan <john@homsar.com>
2014-07-11 17:28:26 -0400

    Initial Commit

---
+++ app/src/main/java/situationalinertia/com/showaduck/showduck.java
@@ -0,0 +1,47 @@
+package situationalinertia.com.showaduck;
+
+/**
+ * Created by jgilman0 on 7/2/2014.
+ */
+
+import android.app.Activity;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+
+public class showduck extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showaduck);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.duckbutton);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent browserIntent =
+                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://commons.wikimedia.org/wiki/File:Bucephala-albeola-010.jpg"));
+                startActivity(browserIntent);
+
+            }
+
+        });
+
+    }
+
+}
